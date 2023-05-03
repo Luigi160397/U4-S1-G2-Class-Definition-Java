41bc3c6f0121ab9e4d8d4d18ea053442fec15ed5
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-03 16:55:42 +0200

    test

---
+++ src/Carrello.java
@@ -0,0 +1,13 @@
+
+public class Carrello {
+	String cliente;
+	Articolo[] articoli;
+	double totCarrello;
+	
+	public Carrello (String cliente, Articolo[] articoli, double totCarrello) {
+		this.cliente = cliente;
+		this.articoli = articoli;
+		this.totCarrello = totCarrello;
+		
+	}
+}
---
+++ src/Esercizio3.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Esercizio3 {
+
+	public static void main(String[] args) {
+		Articolo articolo1 = new Articolo("123asd", "bello",35.99, 10 );
+		Articolo articolo2 = new Articolo("12345asdfgh", "bellissimo",40.99, 15 );
+		
+		Articolo[] listaArticoli = {articolo1, articolo2};
+		
+		
+		Cliente cliente1 = new Cliente("123789qwe", "Giacomo Poretti", "giacomo.poretti@gmail.com", new Date());
+		Cliente cliente2 = new Cliente("123789qwerty", "Signor Rezzonico", "sig.rezzonico@gmail.com", new Date());
+		
+		Carrello carrello1 = new Carrello (cliente1.codCliente, listaArticoli,  );
+	}
+
+	public static double sommaArticoli (double[] lista) {
+		return
+	}
+}
