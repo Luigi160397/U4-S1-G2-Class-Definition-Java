01a39f4a588c4d24adbce114ab36c0b4affc98c2
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-03 17:42:12 +0200

    long

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -3,12 +3,12 @@ public class Esercizio2 {
 
 	public static void main(String[] args) {
 		
-		Chiamata call1 = new Chiamata(2.45,392449433);
+		Chiamata call1 = new Chiamata(2.45,3924494333l);
 		Chiamata call2 = new Chiamata(3.50,521);
 		
 		Chiamata[] listaChiamate1 = {call1, call2};
 		
-		Sim sim1 = new Sim(39244943,30.20, listaChiamate1);
+		Sim sim1 = new Sim(3924494333l,30.20, listaChiamate1);
 		Sim sim2 = new Sim(313312, 5.30);
 		Sim sim3 = new Sim(313312);
 		sim1.stampaSim();
